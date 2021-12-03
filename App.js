19c879fd60ab9515e8cd84de1df87489f4238c1b
fabriciops <fabriciopps19@gmail.com>
2021-12-03 00:29:23 -0300

    Ajustes style e if

--- App.js
+++ App.js
@@ -1,31 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, View, FlatList, Image, ActivityIndicator } from 'react-native';
 
 const App = () => {
 
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  const handleLoadButton = async () => {
-    const req = await fetch("https://api.b7web.com.br/cinema/");
-    const json = await req.json();
-
-    if(json){
-      setMovies(json);
+  useEffect(()=>{
+    const requestMovies = async()=>{
+      setLoading(true);
+      const req = await fetch("https://api.b7web.com.br/cinema/");
+      const json = await req.json();
+  
+      if(json){
+        setMovies(json);
+      }
+      setLoading(false);
     }
 
-  }
+    requestMovies();
+
+  }, []);
+
+  // const handleLoadButton = async () => {
+  //   const req = await fetch("https://api.b7web.com.br/cinema/");
+  //   const json = await req.json();
+
+  //   if(json){
+  //     setMovies(json);
+  //   }
+
+  // }
 
   return (
-    <SafeAreaView>
-      <Button title="Carregar Filmes" onPress={handleLoadButton} />
-      <Text>Total de Filmes: {movies.length}</Text>
-      <FlatList 
+    <SafeAreaView style={styles.container}>
+      {/* <Button title="Carregar Filmes" onPress={handleLoadButton} style={styles.button}/> */}
+
+    {loading && 
+      <View style={styles.loadingArea}>
+        <ActivityIndicator size="large" color="#FFFFFF"/>
+        <Text style={styles.loadingText}> Carregando</Text>
+      </View>
+    }
+
+
+    {!loading && 
+      <Text style={styles.totalMovieText}>Total de Filmes: {movies.length} </Text>>
+      <FlatList
+        style={styles.list} 
         data={movies}
         renderItem = {({item}) => (
-          <View>
-            <Image source={{uri: item.avatar}} style={{width: 200, height:200}}/>
-            <Text>
+          <View style={styles.movieItem}>
+            
+            <Image 
+              source={{uri: item.avatar}}
+              style={styles.movieImage}
+              resizeMode="contain"
+              />
+            <Text style={styles.movieTitle}>
             {item.titulo}
             </Text>
           </View>
@@ -33,8 +66,58 @@ const App = () => {
         keyExtractor={item => item.titulo}
 
       />
+      
+    }
+
+      
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:'#333'
+  },
+  totalMovieText:{
+    color:'#FFF',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10
+
+  },
+  button:{
+    marginTop: 200,
+  },
+  list:{
+    flex: 1
+  },
+  movieItem:{
+    marginBottom: 30
+  },
+  movieImage:{
+    height: 200
+  },
+  movieTitle:{
+    color: '#FFF',
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 5
+  },
+
+  loadingArea:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  loadingText:{
+    color: '#FFF'
+  }
+
+
+
+});
 
 export default App;
--- README.md
+++ README.md
@@ -14,3 +14,25 @@ parametro a após start do sistema
 
 CASO ESTEJA COM O CELULAR CONECTADO
 sudo expo start -c --android
+
+TMDb offers a powerful API service that is free to use as long as you properly attribute us as the source of the data and/or images you use. You can find the logos for attribution here.
+
+Documentation
+Our primary documentation is located at developers.themoviedb.org.
+
+Support
+If you have questions or comments about the information covered here, please create a post on our support forums.
+
+API Details
+If you'd like to edit the details of your app, click here.
+
+App Directory
+Once you have completed your application, add it to the app directory!
+
+API Key (v3 auth)
+baac6d76945f366ba92402ac5179a2a2
+Example API Request
+https://api.themoviedb.org/3/movie/550?api_key=baac6d76945f366ba92402ac5179a2a2
+API Read Access Token (v4 auth)
+
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYWFjNmQ3Njk0NWYzNjZiYTkyNDAyYWM1MTc5YTJhMiIsInN1YiI6IjYxYTk3YzE5Mzg0NjlhMDA5NmQwZDQ3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fK8bXMYU7jH8KfWwVRXu0PSADAvJEu54auXzdvBkWeg
